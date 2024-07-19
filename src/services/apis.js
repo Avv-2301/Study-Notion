68ddfae83f7bbf2bd633d979c783754eb622c4d2
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-19 21:41:09 +0530

    Update api

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://study-notion-backend-2nuo.onrender.co"
+const BASE_URL = "https://study-notion-backend-2nuo.onrender.co/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
