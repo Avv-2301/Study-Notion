7b5d14fc524e2e1b34d1d1164a3a40df752a89be
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-20 15:25:32 +0530

    Update index

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-frontend-puce.vercel.app/",
+		origin:"https://study-notion-frontend-puce.vercel.app",
 		methods: ["GET", "POST", "PUT", "DELETE"],
     	allowedHeaders: ["Content-Type", "Authorization"],
 		credentials:true,
