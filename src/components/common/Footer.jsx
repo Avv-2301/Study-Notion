b7c1da37abf5e8c0b533c5af9e5672d848b3d20d
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-23 11:15:46 +0530

    Initial Commit

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ By Akshat © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ By Akshat © 2024 Studynotion</div>
         </div>
       </div>
     </div>
